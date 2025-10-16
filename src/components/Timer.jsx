a2544b943f8d36250aac7abef38eac8fe87707b5
Velarken <181585418+Velarken@users.noreply.github.com>
2025-10-16 12:10:05 -0400

    Created Timer component
    -- Basic countdown logic added
    -- Added basic timer end screen
    -- Added button to return to timer after breaks

---
+++ src/components/Timer.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react'
+
+export function Timer() {
+    const [timerRunning,setTimerRunning] = useState(false);
+    const [timerFinished,setTimerFinished] = useState(false)
+    const [secondsLeft,setSecondsLeft] = useState(0);
+    const [minutesLeft,setMinutesLeft] = useState(25);
+
+    useEffect(() => {
+        if (timerRunning) {
+            const intervalID = setInterval(() => {
+                if (secondsLeft > 0) {
+                    setSecondsLeft((secondsLeft) => secondsLeft -1)
+                } else {
+                    if (minutesLeft === 0) {
+                        clearInterval(intervalID);
+                        setTimerRunning(false);
+                        setTimerFinished(true);
+                    } else {
+                        setMinutesLeft((minutesLeft) => minutesLeft - 1)
+                        setSecondsLeft(59)
+                    }
+                }
+            }, 1000);
+            return () => clearInterval(intervalID)
+        }
+    }, [timerRunning,minutesLeft,secondsLeft])
+
+    const handleTimerStart = () => {
+        if (secondsLeft <= 0 && minutesLeft <= 0) {
+            alert('Please set timer to a higher value!')
+        }
+        setTimerFinished(false);
+        setTimerRunning(true)
+    }
+    const handleTimerRestart = () => {
+        setTimerFinished(false)
+    }
+    const handleIncrement = () => {
+        setMinutesLeft((minutesLeft) => minutesLeft + 1)
+    }
+    const handleDecremement = () => {
+        setMinutesLeft((minutesLeft) => minutesLeft - 1)
+    }
+    const handleTimerInput = (e) => {
+        setMinutesLeft(e.target.value)
+    }
+
+    return (
+        <>
+            {!timerFinished ?
+                <div className="timer">
+                    <div className="timerDisplay">
+                        {minutesLeft + ' minutes : ' + secondsLeft + ' seconds'}
+                    </div>
+                    <div className="timerButtons">
+                        <button onClick={handleIncrement}>+</button>
+                        <button onClick={handleTimerStart}>Start Timer</button>
+                        <button onClick={handleDecremement}>-</button>
+                    </div>
+                    <div className="timerInput">
+                        <input type='number' onChange={(e) => handleTimerInput(e)}/>
+                    </div>
+                </div>
+            : 
+                <div className="restartTimer">
+                    <h2>Take a quick break before getting back to it! </h2>
+                    <button onClick={handleTimerRestart}>Start another Timer</button>
+                </div>
+            }
+        </>
+    )
+}
+export default Timer
